6391ed3eb074ac9992697b512d04751ebe54ee0f
AjayThandavamurthy <125175600+AjayThandavamurthy@users.noreply.github.com>
2024-11-28 21:40:29 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,23 +1,9 @@
-# import matplotlib.pyplot as plt
-# import cv2
-# import easyocr
-# from IPython.display import Image
-
-# Image("plates/scaned_img_0.jpg")
-
-# reader = easyocr.Reader(['en'])
-
-# output = reader.readtext('plates/scaned_img_0.jpg')
-# output 
 
 import easyocr
 
-# plt.imshow(cv2.imread("plates/scaned_img_0.jpg"))
-# plt.show()
-
 reader = easyocr.Reader(['en'])
 
-output = reader.readtext('plates/scanned_img_20240907_185722.jpg')
+output = reader.readtext('plate.jpg')
 # print(output)
 for box, text, confidence in output:
     print(text)
